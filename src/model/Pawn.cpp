abfd92561608e99f382e219c15e38d16ab1f9d83
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-11-14 15:40:55 +0100

    create pawn structure definition

--- src/model/Pawn.cpp
+++ src/model/Pawn.cpp
@@ -1,6 +1,18 @@
 //
 // Created by Filip Dabkowski on 12/11/2023.
 //
-
 // TODO: Create a structure for pawn (color?, *player?, location?, isHome, isOnBar, canMoveDirection?, canBeRemoved
 // Maybe keep pawn color as enum?
+
+enum PawnColor {
+    WHITE,
+    RED
+};
+
+typedef struct {
+    PawnColor color;
+    int *owner;
+    bool isHome;
+    bool isOnBar;
+    const short moveDirection;
+} Pawn;
