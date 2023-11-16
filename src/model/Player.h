6644477a47770835b22095f5f87482ddef1201ec
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-11-16 17:27:57 +0100

    create player structure

--- src/model/Player.h
+++ src/model/Player.h
@@ -5,4 +5,18 @@
 #ifndef BACKGAMMONGAME_PLAYER_H
 #define BACKGAMMONGAME_PLAYER_H
 
+#define N_DICES 2
+
+#include "string"
+
+typedef unsigned int uint;
+
+typedef struct {
+    int id;
+    std::string name;
+    bool isHisTurn;
+    uint  points;
+    int *numbersRolled[N_DICES];
+} Player;
+
 #endif //BACKGAMMONGAME_PLAYER_H
