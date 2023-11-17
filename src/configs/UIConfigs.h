a0d34dc5b92df08edd8a82fd1766003755165139
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-11-17 01:20:46 +0100

    create configuration .h files

---
+++ src/configs/UIConfigs.h
@@ -0,0 +1,15 @@
+//
+// Created by Filip Dabkowski on 17/11/2023.
+//
+
+#ifndef BACKGAMMONGAME_UICONFIGS_H
+#define BACKGAMMONGAME_UICONFIGS_H
+
+// ncurses does not allow creating custom colour names so let's redefine their colors to yours to standardize the code
+#define COLOUR_MAIN COLOR_GREEN
+#define COLOUR_MAIN_LIGHT COLOR_BLUE
+#define COLOUR_MAIN_DARK COLOR_CYAN
+
+const float colorContrast = 0.1;
+
+#endif //BACKGAMMONGAME_UICONFIGS_H
