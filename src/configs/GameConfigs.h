49d632cf357444d7cadeb96109dfca8c8e18a184
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-11-18 22:49:22 +0100

    #def and const for menu options

--- src/configs/GameConfigs.h
+++ src/configs/GameConfigs.h
@@ -5,6 +5,8 @@
 #ifndef BACKGAMMONGAME_GAMECONFIGS_H
 #define BACKGAMMONGAME_GAMECONFIGS_H
 
+#include "Utility.h"
+
 #define N_BOARDS 2
 #define POINTS_PER_BOARD 6
 #define PAWNS_PER_POINT 5
@@ -13,7 +15,8 @@
 #define PLAYERS_PER_GAME 2
 #define N_DICES 2
 
-typedef unsigned int uint;
+#define N_MENU_OPTIONS 2
+#define MENU_ELEMENT_LEN 6
 
 const uint nPoints = N_BOARDS * POINTS_PER_BOARD;
 const uint totalPawns = N_PLAYERS * PAWNS_PER_PLAYER;
