be8bb40fef311a406e77cfb663e8263aaaf2b52d
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-11-23 23:49:58 +0100

    reformat move checks

--- src/model/Pawn.cpp
+++ src/model/Pawn.cpp
@@ -7,16 +7,22 @@
 
 bool canBeMoved(Board *game, int pieceIndex, int moveBy) {
 	// Index out of range
-	if (pieceIndex >= nPoints || pieceIndex < 0) return false;
+	if (pieceIndex >= nPoints || pieceIndex < 0)
+		return false;
 	// Moving Pawn from an empty Point
-	if (game->points[pieceIndex].pawnsInside == 0) return false;
+	if (game->points[pieceIndex].pawnsInside == 0)
+		return false;
 	// Moving not your Pawn
-	if (*game->points[pieceIndex].pawns[0]->owner != *game->currentPlayer) return false;
+	if (*game->points[pieceIndex].pawns[0]->owner != *game->currentPlayer)
+		return false;
+
 	// Handle move in both direction based on Pawn "Color"
 	short direction = game->points[pieceIndex].pawns[0]->moveDirection;
 	int destinationIndex = pieceIndex + moveBy * direction;
 	// Moving outside of range
-	if (destinationIndex >= nPoints || destinationIndex < 0) return false;
+	if (destinationIndex >= nPoints || destinationIndex < 0)
+		return false;
+
 	return true; // We check all the index Pawn can be moved by player from pos A to B
 }
 
@@ -30,13 +36,17 @@ moveToPoint canMoveTo(Board *game, int fromIndex, int toIndex) {
 	// Consider the destination indexes are already check
 	int destinationSize = game->points[toIndex].pawnsInside;
 	// Moving to full Point
-	if (destinationSize == PAWNS_PER_POINT) return BLOCKED;
+	if (destinationSize == PAWNS_PER_POINT)
+		return BLOCKED;
 	// Check for empty destination Point
-	if (destinationSize == 0) return POSSIBLE;
+	if (destinationSize == 0)
+		return POSSIBLE;
 	// Check if Point is occupied by same player, by now we know the point has at least one empty slot
-	if (*game->points[toIndex].pawns[0]->owner == *game->points[fromIndex].pawns[0]->owner) return POSSIBLE;
+	if (*game->points[toIndex].pawns[0]->owner == *game->points[fromIndex].pawns[0]->owner)
+		return POSSIBLE;
 	// Check if Point is blocked by opponent
-	if (destinationSize > CAPTURE_THRESHOLD) return BLOCKED;
+	if (destinationSize > CAPTURE_THRESHOLD)
+		return BLOCKED;
 	return CAPTURE; // We know Point has an opponents Pawn, but we can Capture it
 }
 
