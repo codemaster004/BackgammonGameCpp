2f7d640c37d02165a918810ecbcbfe5e848b44aa
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-12-10 17:04:18 +0100

    add comments for Player

--- src/model/Player.cpp
+++ src/model/Player.cpp
@@ -14,34 +14,39 @@
 
 // PUBLIC FUNCTION DECLARATIONS //
 
-// PRIVATE FUNCTION DECLARATIONS //
-
 void changePlayers(Board &game) {
+	// Change the current player to the opponent
 	game.currentPlayerId = getOpponent(game, game.currentPlayerId)->id;
 }
 
 Player *getPlayer(Board &game, int playerId) {
+	// Iterate through players to find the one with the matching ID
 	for (auto &player: game.players)
 		if (player.id == playerId)
 			return &player;
 
+	// Return nullptr if no matching player is found
 	return nullptr;
 }
 
 Player *getOpponent(Board &game, int playerId) {
+	// Iterate through players to find the opponent
 	for (auto &player: game.players)
 		if (player.id != playerId)
 			return &player;
 
+	// Since this is a two-player game, this condition will always be met
 	return nullptr;
 }
 
 void serialisePlayer(Player player, uint8_t *buffer, size_t &offset) {
+	// Copy the player structure into the buffer
 	std::memcpy(buffer + offset, &player, sizeof(Player));
 	offset += sizeof(Player);
 }
 
 void serialisePlayerPointer(Player *player, uint8_t *buffer, size_t &offset) {
+	// Serialize the player ID or -1 if the pointer is null
 	if (player == nullptr) {
 		serializeInt(-1, buffer, offset);
 	} else {
@@ -50,30 +55,41 @@ void serialisePlayerPointer(Player *player, uint8_t *buffer, size_t &offset) {
 }
 
 void serializeScorePlayer(ScorePlayer player, uint8_t *buffer, size_t &offset) {
+	// Copy the ScorePlayer structure into the buffer
 	std::memcpy(buffer + offset, &player, sizeof(ScorePlayer));
 	offset += sizeof(ScorePlayer);
 }
 
 Player deserializePlayer(const uint8_t *buffer, size_t &index) {
 	Player player;
+	// Copy data from the buffer into the player structure
 	std::memcpy(&player, buffer + index, sizeof(Player));
 	index += sizeof(Player);
 	return player;
 }
 
 Player *deserializePlayerPointer(Board &board, const uint8_t *buffer, size_t &offset) {
+	// Deserialize the player ID
 	int id = deserializeInt(buffer, offset);
+
+	// Return nullptr if ID is -1, indicating no player
 	if (id == -1)
 		return nullptr;
+
+	// Find and return the player with the deserialized ID
 	for (Player &player: board.players)
 		if (player.id == id)
 			return &player;
+
 	return nullptr;
 }
 
 ScorePlayer deserializeScorePlayer(const uint8_t *buffer, size_t &index) {
 	ScorePlayer player;
+	// Copy data from the buffer into the ScorePlayer structure
 	std::memcpy(&player, buffer + index, sizeof(ScorePlayer));
 	index += sizeof(ScorePlayer);
 	return player;
 }
+
+// PRIVATE FUNCTION DECLARATIONS //
--- src/model/Player.h
+++ src/model/Player.h
@@ -37,24 +37,84 @@ typedef struct ScorePlayer {
 } ScorePlayer;
 
 
+/**
+ * @brief Changes the current player to the opponent.
+ *
+ * @param game Reference to the Board structure representing the game state.
+ */
 void changePlayers(Board &game);
 
+/**
+ * @brief Retrieves a pointer to a Player structure based on its ID.
+ *
+ * @param game Reference to the Board structure.
+ * @param playerId The ID of the player to retrieve.
+ * @return Player* Pointer to the Player structure, or nullptr if not found.
+ */
 Player *getPlayer(Board &game, int playerId);
 
+/**
+ * @brief Retrieves a pointer to the opponent Player structure.
+ *
+ * @param game Reference to the Board structure.
+ * @param playerId The ID of the current player.
+ * @return Player* Pointer to the opponent Player structure.
+ */
 Player *getOpponent(Board &game, int playerId);
 
-/// Handle Serialization of Player object
+/**
+ * @brief Serializes a Player structure into a buffer.
+ *
+ * @param player The Player structure to serialize.
+ * @param buffer Pointer to the buffer for storing serialized data.
+ * @param offset Reference to the size_t variable indicating the current offset in the buffer.
+ */
 void serialisePlayer(Player player, uint8_t *buffer, size_t &offset);
 
-Player deserializePlayer(const uint8_t *buffer, size_t &index);
-
-/// Handle POINTER serialization to a Player object
+/**
+ * @brief Serializes a pointer to a Player structure into a buffer.
+ *
+ * @param player Pointer to the Player structure to serialize.
+ * @param buffer Pointer to the buffer for storing serialized data.
+ * @param offset Reference to the size_t variable indicating the current offset in the buffer.
+ */
 void serialisePlayerPointer(Player *player, uint8_t *buffer, size_t &offset);
 
-Player *deserializePlayerPointer(Board &board, const uint8_t *buffer, size_t &offset);
-
+/**
+ * @brief Serializes a ScorePlayer structure into a buffer.
+ *
+ * @param player The ScorePlayer structure to serialize.
+ * @param buffer Pointer to the buffer for storing serialized data.
+ * @param offset Reference to the size_t variable indicating the current offset in the buffer.
+ */
 void serializeScorePlayer(ScorePlayer player, uint8_t *buffer, size_t &offset);
 
+/**
+ * @brief Deserializes a Player structure from a buffer.
+ *
+ * @param buffer Pointer to the buffer containing serialized data.
+ * @param index Reference to the size_t variable indicating the current index in the buffer.
+ * @return Player The deserialized Player structure.
+ */
+Player deserializePlayer(const uint8_t *buffer, size_t &index);
+
+/**
+ * @brief Deserializes a pointer to a Player structure from a buffer.
+ *
+ * @param board Reference to the Board containing Player structures.
+ * @param buffer Pointer to the buffer containing serialized data.
+ * @param offset Reference to the size_t variable indicating the current offset in the buffer.
+ * @return Player* Pointer to the deserialized Player structure, or nullptr if not found.
+ */
+Player *deserializePlayerPointer(Board &board, const uint8_t *buffer, size_t &offset);
+
+/**
+ * @brief Deserializes a ScorePlayer structure from a buffer.
+ *
+ * @param buffer Pointer to the buffer containing serialized data.
+ * @param index Reference to the size_t variable indicating the current index in the buffer.
+ * @return ScorePlayer The deserialized ScorePlayer structure.
+ */
 ScorePlayer deserializeScorePlayer(const uint8_t *buffer, size_t &index);
 
 #endif //BACKGAMMONGAME_PLAYER_H
